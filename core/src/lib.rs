e5e283cced6927a451f38dc2a573f5c5622ddc09
Willem Victor <wimpievictor@gmail.com>
2022-04-12 21:06:03 +0200

    WIP Python + SFML code (#3)
    * Added build notes to readme
    
    * Fixed module name
    
    * Experimenting with exposing sfml window from rust to python

--- core/src/lib.rs
+++ core/src/lib.rs
@@ -1,3 +1,36 @@
+extern crate sfml;
+
+pub use sfml::window::{Event, Style, Window};
+
+pub fn create_window() {
+    // Create a new window
+    let mut window = Window::new((800, 600), "SFML window", Style::CLOSE, &Default::default());
+    // Limit the framerate to 60 frames per second (this step is optional)
+    window.set_framerate_limit(60);
+    run_loop(window);
+}
+
+pub fn run_loop(mut window: Window) {
+    // The main loop - ends as soon as the window is closed
+    while window.is_open() {
+        // Event processing
+        while let Some(event) = window.poll_event() {
+            // Request closing for the window
+            if event == Event::Closed {
+                window.close();
+            }
+        }
+
+        // Activate the window for OpenGL rendering
+        window.set_active(true);
+
+        // OpenGL drawing commands go here...
+
+        // End the current frame and display its contents on screen
+        window.display();
+    }
+}
+
 pub fn say_hello() {
     println!("Hello world");
 }
--- python/src/lib.rs
+++ python/src/lib.rs
@@ -5,7 +5,19 @@ use pyo3::prelude::*;
 use pyo3::wrap_pyfunction;
 
 #[pyfunction]
+fn create_window() -> PyResult<()> {
+    core::create_window();
+
+    Ok(())
+}
+
+#[pyfunction]
+fn run_loop() -> PyResult<()> {
+    Ok(())
+}
+
 /// Formats the sum of two numbers as string
+#[pyfunction]
 fn say_hello() -> PyResult<()> {
     core::say_hello();
 
@@ -14,8 +26,9 @@ fn say_hello() -> PyResult<()> {
 
 /// This module is a python module implemented in Rust.
 #[pymodule]
-fn core_mod(py: Python, m: &PyModule) -> PyResult<()> {
+fn gers(py: Python, m: &PyModule) -> PyResult<()> {
     m.add_wrapped(wrap_pyfunction!(say_hello))?;
+    m.add_wrapped(wrap_pyfunction!(create_window))?;
 
     Ok(())
 }
